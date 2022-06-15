d22e2e816531d8c42d50bd15afab44bea80cfcd9
JPC8 <joshua.cyril88@gmail.com>
2022-06-15 10:13:11 +0530

    🎉Beautified. ✔ Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
-!(function($) {
+!(function ($) {
     "use strict";
     // Porfolio isotope and filter
-    $(window).on('load', function() {
+    $(window).on('load', function () {
         var blogIsotope = $('.blog-container').isotope({
-        itemSelector: '.blog-item'
+            itemSelector: '.blog-item'
         });
 
-        $('.blog-flt li').on('click', function() {
-        $(".blog-flt li").removeClass('filter-active');
-        $(this).addClass('filter-active');
+        $('.blog-flt li').on('click', function () {
+            $(".blog-flt li").removeClass('filter-active');
+            $(this).addClass('filter-active');
 
-        blogIsotope.isotope({
-            filter: $(this).data('filter')
+            blogIsotope.isotope({
+                filter: $(this).data('filter')
+            });
         });
-        });
-    });       
+    });
 })(jQuery);
